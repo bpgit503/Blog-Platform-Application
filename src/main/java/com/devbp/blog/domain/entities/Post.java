638e2cf2a62bad8334777f552202efe2e333c55c
Bren <bpgit503@gmail.com>
2025-03-23 21:46:24 +0100

    Configured relationship between tag and post enetities

--- src/main/java/com/devbp/blog/domain/entities/Post.java
+++ src/main/java/com/devbp/blog/domain/entities/Post.java
@@ -6,7 +6,9 @@ import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -37,6 +39,11 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "post_tags", joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags = new HashSet<>();
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
--- src/main/java/com/devbp/blog/domain/entities/Tag.java
+++ src/main/java/com/devbp/blog/domain/entities/Tag.java
@@ -1,17 +1,13 @@
 package com.devbp.blog.domain.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 @Entity
-@Table(name = "Tags")
+@Table(name = "tags")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -25,6 +21,9 @@ public class Tag {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Post> posts = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
