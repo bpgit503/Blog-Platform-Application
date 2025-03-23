9469b29675e1a26aea2caaf27b6b668f2eb8cfb5
Bren <bpgit503@gmail.com>
2025-03-23 19:10:15 +0100

    refactored User Entity

--- src/main/java/com/devbp/blog/domain/entities/UserEntity.java
+++ src/main/java/com/devbp/blog/domain/entities/UserEntity.java
@@ -1,21 +1,24 @@
-package com.devbp.blog.entities;
+package com.devbp.blog.domain.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.context.annotation.EnableMBeanExport;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 
 @Entity(name = "users")
-@Data
+@Table(name = "users")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserEntity {
 
     @Id
@@ -23,11 +26,33 @@ public class UserEntity {
     private UUID id;
 
     @Email
-    @Column(name = "email" )
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
+    private String password;
 
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false)
+    private LocalDateTime createAt;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(email, that.email) && Objects.equals(password, that.password) && Objects.equals(name, that.name) && Objects.equals(createAt, that.createAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, name, createAt);
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createAt = LocalDateTime.now();
+    }
 }
 
