a1bc02600da4c78d9e9d870d6d4665d92001f112
Bren <bpgit503@gmail.com>
2025-03-24 19:10:24 +0100

    Configured Test environment to connect to H2 database

--- src/main/java/com/devbp/blog/domain/controllers/ErrorController.java
+++ src/main/java/com/devbp/blog/domain/controllers/ErrorController.java
@@ -4,7 +4,6 @@ import com.devbp.blog.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
