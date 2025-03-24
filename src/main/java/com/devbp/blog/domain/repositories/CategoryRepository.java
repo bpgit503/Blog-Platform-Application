c749f06cfa8750d74ea51f905ee98fb860dc0428
Bren <bpgit503@gmail.com>
2025-03-24 10:12:11 +0100

    Created base repositories for User, Category, Tag, Post classes

---
+++ src/main/java/com/devbp/blog/domain/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.devbp.blog.domain.repositories;
+
+import com.devbp.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
