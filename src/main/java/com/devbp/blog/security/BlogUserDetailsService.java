38aa3cd04395f20d2717d46a19975d9d0b8ca4b1
Bren <bpgit503@gmail.com>
2025-03-28 10:44:51 +0100

    Created LoginRequest and Authentication Response classes to handle and authenticate user logins created Authenticator Controller to handle login in endpoints

--- src/main/java/com/devbp/blog/security/BlogUserDetailsService.java
+++ src/main/java/com/devbp/blog/security/BlogUserDetailsService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
 
 // used by spring security to look up users
 @RequiredArgsConstructor
