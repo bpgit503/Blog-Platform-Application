f4ff24ff50b80a860fa7540a1cf4a8613cf2c839
Bren <bpgit503@gmail.com>
2025-03-28 16:38:44 +0100

    created filter to validate user authentication tokens

--- src/main/java/com/devbp/blog/security/BlogUserDetails.java
+++ src/main/java/com/devbp/blog/security/BlogUserDetails.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 //bridge the gap between the user and the user details
 @Getter
@@ -50,5 +51,9 @@ public class BlogUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public UUID getId() {
+        return user.getId();
     }
 }
